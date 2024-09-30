83bfd30ade2e26a77b5beed236106c1a7400f0e2
AniketSharma-dev <aniketsharma.xo@gmail.com>
2024-09-30 23:52:10 +0530

    Uploding Files

---
+++ app.js
@@ -0,0 +1,26 @@
+gsap.from(".hero-img1", {
+  delay: 0.4,
+  opacity: 0,
+  duration: 1,
+  y: 60,
+});
+
+gsap.from(".hero-img2", {
+  delay: 0.4,
+  opacity: 0,
+  duration: 1,
+  x: 60,
+});
+
+gsap.from(".hero-img3", {
+  delay: 0.4,
+  opacity: 0,
+  duration: 1,
+  y: -60,
+});
+
+gsap.from(".hero-text h1", {
+  delay: 0.4,
+  opacity: 0,
+  duration: 1,
+});
